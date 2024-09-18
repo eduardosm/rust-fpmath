ff7d559efcfe5e55134a9de67854a2b37f81e3eb
Eduardo Sánchez Muñoz <eduardosm-dev@e64.io>
2024-09-18 17:25:34 +0200

    ci: use a loop to run clippy with different features enabled

--- ci/clippy.sh
+++ ci/clippy.sh
@@ -11,10 +11,10 @@ begin_group "Run clippy workspace"
 cargo clippy --frozen --workspace --all-targets  -- -D warnings
 end_group
 
-begin_group "Run clippy fpmath without soft-float"
-cargo clippy --frozen -p fpmath --all-targets -- -D warnings
-end_group
+features_array=("" "soft-float")
 
-begin_group "Run clippy fpmath with soft-float"
-cargo clippy --frozen -p fpmath --all-targets --features "soft-float" -- -D warnings
-end_group
+for features in "${features_array[@]}"; do
+  begin_group "Run clippy fpmath, features=\"$features\""
+  cargo clippy --frozen -p fpmath --all-targets --no-default-features --features "$features" -- -D warnings
+  end_group
+done
