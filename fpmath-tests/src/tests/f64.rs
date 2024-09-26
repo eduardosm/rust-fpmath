73e1f45b2d59301fb0c9824bc8bed6bb31813132
Eduardo Sánchez Muñoz <eduardosm-dev@e64.io>
2024-09-26 17:29:24 +0200

    test: restrict the visibility of some items and remove unneeded `cfg` gates

--- fpmath-tests/src/tests/f64.rs
+++ fpmath-tests/src/tests/f64.rs
@@ -791,7 +791,7 @@ fn test_tanh() {
     assert!(max_error > 0.5);
 }
 
-pub(super) fn test_round_with(test_f: fn(f64)) {
+fn test_round_with(test_f: fn(f64)) {
     use rand::Rng as _;
 
     let mut rng = crate::data::create_prng();
