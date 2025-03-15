9b76e44fa0c5b34ee3d2f6e02c3f8a29754ebe60
Eduardo Sánchez Muñoz <eduardosm-dev@e64.io>
2025-03-15 14:02:04 +0100

    change: try to improve generation of gamma polynomial approximations

--- generator/src/generate/julia.rs
+++ generator/src/generate/julia.rs
@@ -50,14 +50,14 @@ fn run_julia(input: &str) -> Result<Vec<u8>, JuliaError> {
 pub(super) fn run_and_render_remez(
     fkind: FloatKind,
     func: &str,
+    wfunc: &str,
     range: (f64, f64),
     poly_deg: i32,
     poly_i_print_off: i32,
     coeff_prefix: &str,
-    exclude_coeffs: &[i32],
     out: &mut String,
 ) {
-    let code = gen_remez_code(func, range, poly_deg);
+    let code = gen_remez_code(func, wfunc, range, poly_deg);
     let result = run_julia(&code).unwrap();
 
     let mut lines = result.split(|&c| c == b'\n');
@@ -71,47 +71,45 @@ pub(super) fn run_and_render_remez(
         match fkind {
             FloatKind::F32 => {
                 let coeff_value = parse_f32(coeff_line);
-                if !exclude_coeffs.contains(&i) {
-                    writeln!(
-                        out,
-                        "const {coeff_prefix}{}: u32 = 0x{:08X}; // {coeff_value:e}",
-                        i + poly_i_print_off,
-                        coeff_value.to_bits(),
-                    )
-                    .unwrap();
-                }
+                writeln!(
+                    out,
+                    "const {coeff_prefix}{}: u32 = 0x{:08X}; // {coeff_value:e}",
+                    i + poly_i_print_off,
+                    coeff_value.to_bits(),
+                )
+                .unwrap();
             }
             FloatKind::F64 => {
                 let coeff_value = parse_f64(coeff_line);
-                if !exclude_coeffs.contains(&i) {
-                    writeln!(
-                        out,
-                        "const {coeff_prefix}{}: u64 = 0x{:016X}; // {coeff_value:e}",
-                        i + poly_i_print_off,
-                        coeff_value.to_bits(),
-                    )
-                    .unwrap();
-                }
+                writeln!(
+                    out,
+                    "const {coeff_prefix}{}: u64 = 0x{:016X}; // {coeff_value:e}",
+                    i + poly_i_print_off,
+                    coeff_value.to_bits(),
+                )
+                .unwrap();
             }
         }
     }
 }
 
-fn gen_remez_code(func: &str, range: (f64, f64), poly_deg: i32) -> String {
+fn gen_remez_code(func: &str, wfunc: &str, range: (f64, f64), poly_deg: i32) -> String {
     let mut code = String::new();
 
     code.push_str("import Remez;\n");
-    code.push_str("import Printf;\n");
     code.push_str("import SpecialFunctions;\n");
-    code.push_str("Remez.setprecision(BigFloat, 512);\n");
+    code.push_str("Remez.setprecision(BigFloat, 1024);\n");
     code.push_str("f = (x) -> ");
     code.push_str(func);
+    code.push_str(";\n");
+    code.push_str("w = (x, fx) -> ");
+    code.push_str(wfunc);
     code.push_str(";\n");
 
     code.push_str("N, D, E, X = Remez.ratfn_minimax(f, ");
     write!(code, "({}, {}), ", range.0, range.1).unwrap();
     write!(code, "{poly_deg},").unwrap();
-    code.push_str(" 0);\n");
+    code.push_str(" 0, w);\n");
 
     code.push_str("println(E);\n");
     for i in 1..=(poly_deg + 1) {
--- src/f32/gamma.rs
+++ src/f32/gamma.rs
@@ -46,16 +46,16 @@ impl<F: F32Like> crate::generic::Gamma<LikeF32> for F {
         // GENERATE: gamma::lgamma_poly_1 f32 12 1.2
         const K1: u32 = 0xBF13C468; // -5.772157e-1
         const K2: u32 = 0x3F528D34; // 8.224671e-1
-        const K3: u32 = 0xBECD2724; // -4.0068924e-1
-        const K4: u32 = 0x3E8A8775; // 2.705647e-1
-        const K5: u32 = 0xBE541CDC; // -2.0714134e-1
-        const K6: u32 = 0x3E2EF709; // 1.7086424e-1
-        const K7: u32 = 0xBE18A740; // -1.4907551e-1
-        const K8: u32 = 0x3DAD637B; // 8.46624e-2
-        const K9: u32 = 0xBE052FA3; // -1.3006453e-1
-        const K10: u32 = 0x3EFA602D; // 4.89015e-1
-        const K11: u32 = 0x3F7C4B16; // 9.855207e-1
-        const K12: u32 = 0x3F85D08F; // 1.0454272e0
+        const K3: u32 = 0xBECD26B9; // -4.0068606e-1
+        const K4: u32 = 0x3E8A884D; // 2.7057114e-1
+        const K5: u32 = 0xBE54530B; // -2.0734803e-1
+        const K6: u32 = 0x3E2E5770; // 1.7025542e-1
+        const K7: u32 = 0xBE13CD6B; // -1.443383e-1
+        const K8: u32 = 0x3DD8DEBF; // 1.05893604e-1
+        const K9: u32 = 0xBE16B9B5; // -1.4719279e-1
+        const K10: u32 = 0x3E867A2E; // 2.626509e-1
+        const K11: u32 = 0x3F0F88B0; // 5.6067944e-1
+        const K12: u32 = 0x3F49DECC; // 7.8855586e-1
 
         let k1 = Self::from_raw(K1);
         let k2 = Self::from_raw(K2);
@@ -79,16 +79,16 @@ impl<F: F32Like> crate::generic::Gamma<LikeF32> for F {
         // GENERATE: gamma::lgamma_poly_2 f32 12 1.2 2.3
         const K1: u32 = 0x3ED87730; // 4.2278433e-1
         const K2: u32 = 0x3EA51A66; // 3.2246703e-1
-        const K3: u32 = 0xBD89F004; // -6.7352325e-2
-        const K4: u32 = 0x3CA89909; // 2.0580785e-2
-        const K5: u32 = 0xBBF1FC4B; // -7.384812e-3
-        const K6: u32 = 0x3B3D8928; // 2.8920863e-3
-        const K7: u32 = 0xBA9D5730; // -1.2004133e-3
-        const K8: u32 = 0x39FCDFA6; // 4.8231817e-4
-        const K9: u32 = 0xB96522B2; // -2.1852067e-4
-        const K10: u32 = 0x3971F78E; // 2.3075772e-4
-        const K11: u32 = 0x392E52A9; // 1.6624726e-4
-        const K12: u32 = 0x391420D1; // 1.4126605e-4
+        const K3: u32 = 0xBD89F002; // -6.735231e-2
+        const K4: u32 = 0x3CA89911; // 2.05808e-2
+        const K5: u32 = 0xBBF1FE68; // -7.385064e-3
+        const K6: u32 = 0x3B3D7FFF; // 2.8915403e-3
+        const K7: u32 = 0xBA9D2771; // -1.1989904e-3
+        const K8: u32 = 0x39FF98F1; // 4.875134e-4
+        const K9: u32 = 0xB9632A1C; // -2.1664094e-4
+        const K10: u32 = 0x3967F98E; // 2.2122843e-4
+        const K11: u32 = 0x3920E4F8; // 1.5344087e-4
+        const K12: u32 = 0x390EFD91; // 1.3636636e-4
 
         let k1 = Self::from_raw(K1);
         let k2 = Self::from_raw(K2);
@@ -113,12 +113,12 @@ impl<F: F32Like> crate::generic::Gamma<LikeF32> for F {
         const K0: u32 = 0x3DAAAAAB; // 8.3333336e-2
         const K1: u32 = 0x3B638E3A; // 3.4722225e-3
         const K2: u32 = 0xBB2FB999; // -2.6813506e-3
-        const K3: u32 = 0xB96FF798; // -2.2885052e-4
-        const K4: u32 = 0x3A4B562E; // 7.756677e-4
-        const K5: u32 = 0x390C9519; // 1.3406984e-4
-        const K6: u32 = 0xBA68C85B; // -8.879953e-4
-        const K7: u32 = 0x3A43DE66; // 7.4717996e-4
-        const K8: u32 = 0xB96EF373; // -2.278814e-4
+        const K3: u32 = 0xB96FF7BD; // -2.2885106e-4
+        const K4: u32 = 0x3A4B568E; // 7.756733e-4
+        const K5: u32 = 0x390C8CD7; // 1.3403907e-4
+        const K6: u32 = 0xBA68C21E; // -8.8790234e-4
+        const K7: u32 = 0x3A43D4AB; // 7.4703497e-4
+        const K8: u32 = 0xB96EDAFB; // -2.2779025e-4
 
         let k0 = Self::from_raw(K0);
         let k1 = Self::from_raw(K1);
