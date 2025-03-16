feb83b9b512579735ab88c849ef5db2cc728a260
Eduardo Sánchez Muñoz <eduardosm-dev@e64.io>
2025-03-16 18:20:20 +0100

    refactor(generator): de-duplicate code in "reduce_pi_2_large" constants generation

--- generator/src/generate/data/reduce_pi_2_large.rs
+++ generator/src/generate/data/reduce_pi_2_large.rs
@@ -8,30 +8,9 @@ pub(in super::super) fn gen_frac_2_pi_large(args: &[&str]) -> Result<String, Str
     let mut out = String::new();
 
     let num_words = 66;
-    let mut tmp = 2u8 / rug::Float::with_val(num_words * 24, rug::float::Constant::Pi);
+    let tmp = 2u8 / rug::Float::with_val(num_words * 24, rug::float::Constant::Pi);
 
-    out.push_str("const FRAC_2_PI_LARGE: &[u32] = &[\n");
-    let words_per_line = 9;
-    for i in 0..num_words {
-        if (i % words_per_line) == 0 {
-            out.push_str("    ");
-        } else {
-            out.push(' ');
-        }
-        tmp <<= 24;
-        let word = tmp
-            .to_u32_saturating_round(rug::float::Round::Zero)
-            .unwrap();
-        write!(out, "0x{word:06X},").unwrap();
-        tmp -= word;
-        if (i % words_per_line) == (words_per_line - 1) {
-            out.push('\n');
-        }
-    }
-    if (num_words % words_per_line) != 0 {
-        out.push('\n');
-    }
-    out.push_str("];\n");
+    render_const_24bit_words("FRAC_2_PI_LARGE", tmp, num_words, &mut out);
 
     Ok(out)
 }
@@ -43,9 +22,18 @@ pub(in super::super) fn gen_frac_pi_2_medium(args: &[&str]) -> Result<String, St
 
     let num_words = 8;
     let mut tmp = rug::Float::with_val(num_words * 24, rug::float::Constant::Pi) / 2u8;
-
-    out.push_str("const FRAC_PI_2_MEDIUM: &[u32] = &[\n");
     tmp /= 2u8;
+
+    render_const_24bit_words("FRAC_PI_2_MEDIUM", tmp, num_words, &mut out);
+
+    Ok(out)
+}
+
+fn render_const_24bit_words(name: &str, mut tmp: rug::Float, num_words: u32, out: &mut String) {
+    out.push_str("const ");
+    out.push_str(name);
+    out.push_str(": &[u32] = &[\n");
+
     let words_per_line = 9;
     for i in 0..num_words {
         if (i % words_per_line) == 0 {
@@ -55,7 +43,10 @@ pub(in super::super) fn gen_frac_pi_2_medium(args: &[&str]) -> Result<String, St
         }
         tmp <<= 24;
         let word = tmp
-            .to_u32_saturating_round(rug::float::Round::Zero)
+            .to_integer_round(rug::float::Round::Zero)
+            .unwrap()
+            .0
+            .to_u32()
             .unwrap();
         write!(out, "0x{word:06X},").unwrap();
         tmp -= word;
@@ -67,6 +58,4 @@ pub(in super::super) fn gen_frac_pi_2_medium(args: &[&str]) -> Result<String, St
         out.push('\n');
     }
     out.push_str("];\n");
-
-    Ok(out)
 }
