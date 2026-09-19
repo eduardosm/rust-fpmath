da0fcbbf0e6dcbd7a73a2f795b18bf4ecdbf5287
Eduardo Sánchez Muñoz <eduardosm-dev@e64.io>
2026-09-19 12:47:03 +0200

    ci: test i586 only in release mode
    i586 gives trouble mainly in optimized builds, since that is the case where intermediary results are mostly kept in 80-bit registers

--- ci/test-i586.sh
+++ ci/test-i586.sh
@@ -7,10 +7,6 @@ begin_group "Fetch dependencies"
 cargo fetch --locked
 end_group
 
-begin_group "Test i586 debug"
-cargo test --frozen --workspace --target i586-unknown-linux-gnu
-end_group
-
 begin_group "Test i586 release"
 cargo test --frozen --workspace --target i586-unknown-linux-gnu --release
 end_group
